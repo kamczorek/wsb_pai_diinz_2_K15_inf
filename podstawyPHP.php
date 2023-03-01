0d420b3bd3c322c6d62d919fbc201fae5a4dc78a
Kamil <wkamil425@gmail.com>
2023-03-01 13:38:32 +0100

    add file podstawyPHP.php

---
+++ podstawyPHP.php
@@ -0,0 +1,9 @@
+<?php
+	$firstName = "Janusz";
+	$lastName = "Janeruszewski";
+	echo "Imię i nazwisko: $firstName $lastName<br>";
+	echo 'Imię i nazwisko: $firstName $lastName<br>';
+
+	//phpstorm | VC
+?>
+	
